e03ca2278c9745d2e6e28f9d3a9298304ed2c910
Kagami Hiiragi <kagami@genshiken.org>
2018-03-29 18:10:00 +0300

    Test latin names

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-/* global describe, it */
+/* global describe, context, beforeEach, it */
 
 const fs = require("fs");
 const path = require("path");
@@ -6,12 +6,33 @@ const assert = require("assert");
 const ruhangul = require("../");
 
 describe("ruhangul", () => {
-  it("should transliterate idol names", () => {
-    const fpath = path.join(__dirname, "fixtures", "idols.txt");
-    const data = fs.readFileSync(fpath, {encoding: "utf-8"});
-    data.trim().split(/\n+/).forEach((line) => {
-      const [, input, output] = line.match(/(.*) -> (.*)/);
-      assert.equal(ruhangul.name(input), output);
+  describe("fixtures", () => {
+    beforeEach(function(done) {
+      const fname = `${this.currentTest.parent.title}.txt`;
+      const fpath = path.join(__dirname, "fixtures", fname);
+      fs.readFile(fpath, {encoding: "utf-8"}, (err, data) => {
+        if (err != null) return done(err);
+        this.currentTest.ctx.fixture = data.trim().split(/\n+/).map((line) =>
+          line.match(/(.*) -> (.*)/).slice(1)
+        );
+        done();
+      });
+    });
+
+    context("names-3syl-ru", () => {
+      it("should transliterate full names to cyrillic", function() {
+        this.fixture.forEach(([input, output]) => {
+          assert.equal(ruhangul.name(input), output);
+        });
+      });
+    });
+
+    context("names-3syl-en", () => {
+      it("should transliterate full names to latin", function() {
+        this.fixture.forEach(([input, output]) => {
+          assert.equal(ruhangul.enname(input), output);
+        });
+      });
     });
   });
 });
